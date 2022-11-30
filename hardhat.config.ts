4ac7933af53878d8ad9837914145144b01ce0690
Thomas Coratger <tcoratger@MacBook-Pro-de-Thomas.local>
2022-11-30 22:00:22 +0100

    nft minting smart contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -89,7 +89,7 @@ const config: HardhatUserConfig = {
   },
   namedAccounts: {
     owner: {
-      default: "0xbD4CAF9E8aBC11bFeBba6f12c408144621f76949",
+      default: "0xB201fDd90b14cc930bEc2c4E9f432bC1CA5Ad7C5", //"0xbD4CAF9E8aBC11bFeBba6f12c408144621f76949",
       31337: 0,
     },
     operator: {
---
+++ scripts/deploy.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat";
+
+
+async function main() {
+
+    const NFTMint = await ethers.getContractFactory("NFTMint")
+
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await NFTMint.deploy()
+    await myNFT.deployed()
+    console.log("Contract deployed to address:", myNFT.address)
+}
+
+main()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.error(error)
+    process.exit(1)
+})
+  
