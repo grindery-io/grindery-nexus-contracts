8725c24ebd2648d1674de58679621d6fa3ce6047
sam <ayorindesam2@gmail.com>
2022-12-10 12:03:51 +0100

    Metadata contract

---
+++ contracts/ERC721MetadataStorage.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (token/ERC721/extensions/ERC721URIStorage.sol)
+
+pragma solidity ^0.8.0;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import '@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol';
+
