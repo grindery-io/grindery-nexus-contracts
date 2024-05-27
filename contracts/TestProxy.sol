2606d9a3eba2ea57689d97bec806a6ed4fb95161
SAPikachu <sapikachu@gmail.com>
2024-05-27 19:33:08 +0800

    Update dependencies

--- contracts/TestProxy.sol
+++ contracts/TestProxy.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.24;
+pragma solidity 0.8.25;
 
 import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -134,7 +134,7 @@ const config: HardhatUserConfig = {
     },
   },
   solidity: {
-    version: "0.8.24",
+    version: "0.8.25",
     settings: {
       optimizer: {
         enabled: true,
