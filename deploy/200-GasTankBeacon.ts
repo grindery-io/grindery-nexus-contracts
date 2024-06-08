df4537a2ba0cd2499f7fd6798de9f9d95ad0f035
SAPikachu <sapikachu@gmail.com>
2024-06-08 14:26:01 +0800

    ERC1967Stub is not necessary

--- deploy/200-GasTankBeacon.ts
+++ deploy/200-GasTankBeacon.ts
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { getGasConfiguration } from "../lib/gas";
+import assert from "assert";
 
 const DEPLOYMENT_NAME = "GasTankBeacon";
 
@@ -8,12 +9,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, deployments, ethers } = hre;
   const { deploy } = deployments;
   const { owner } = await getNamedAccounts();
-  const stub = await deployments.get("ERC1967Stub");
 
-  const result = await deploy(DEPLOYMENT_NAME, {
+  assert(typeof hre.config.deterministicDeployment === "function", "deterministicDeployment is not set");
+
+  await deploy(DEPLOYMENT_NAME, {
     contract: "UpgradeableBeacon",
     from: owner,
-    args: [stub.address, owner],
+    args: [hre.config.deterministicDeployment(hre.network.name)?.factory, owner],
     log: true,
     estimateGasExtra: 10000,
     deterministicDeployment: ethers.keccak256(ethers.getBytes(ethers.toUtf8Bytes(DEPLOYMENT_NAME))),
@@ -24,5 +26,5 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 };
 func.id = DEPLOYMENT_NAME;
 func.tags = [DEPLOYMENT_NAME];
-func.dependencies = ["ERC1967Stub"];
+func.dependencies = [];
 export default func;
--- deploy/210-FeeAccountantPrimaryBeacon.ts
+++ deploy/210-FeeAccountantPrimaryBeacon.ts
@@ -1,6 +1,7 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { DeployFunction } from "hardhat-deploy/types";
 import { getGasConfiguration } from "../lib/gas";
+import assert from "assert";
 
 const DEPLOYMENT_NAME = "FeeAccountantPrimaryBeacon";
 
@@ -11,12 +12,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { getNamedAccounts, deployments, ethers } = hre;
   const { deploy } = deployments;
   const { owner } = await getNamedAccounts();
-  const stub = await deployments.get("ERC1967Stub");
+
+  assert(typeof hre.config.deterministicDeployment === "function", "deterministicDeployment is not set");
 
   await deploy(DEPLOYMENT_NAME, {
     contract: "UpgradeableBeacon",
     from: owner,
-    args: [stub.address, owner],
+    args: [hre.config.deterministicDeployment(hre.network.name)?.factory, owner],
     log: true,
     estimateGasExtra: 10000,
     deterministicDeployment: ethers.keccak256(ethers.getBytes(ethers.toUtf8Bytes(DEPLOYMENT_NAME))),
@@ -27,5 +29,5 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 };
 func.id = DEPLOYMENT_NAME;
 func.tags = [DEPLOYMENT_NAME];
-func.dependencies = ["ERC1967Stub"];
+func.dependencies = [];
 export default func;
