095f85573cb87674e343a92eb873105c87ed48a8
cometsheepboy <cometsheepboy@gmail.com>
2025-05-09 06:29:22 -0500

    [fix] update

--- contracts/AIGasTank.sol
+++ contracts/AIGasTank.sol
@@ -3,22 +3,45 @@
 pragma solidity 0.8.25;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-
-contract AIGasTank is ReentrancyGuard, Ownable {
+import "@openzeppelin/contracts/utils/math/SafeCast.sol";
+struct FeeReport {
+    address user;
+    address agent;
+    uint256 amount;
+}
+contract AIGasTank is
+    Initializable,
+    OwnableUpgradeable,
+    AccessControlUpgradeable,
+    ReentrancyGuarde
+{
     using SafeERC20 for IERC20;
+
+    bytes32 public constant ROLE_OPERATOR = keccak256("ROLE_OPERATOR");
 
     event Deposit(address indexed user, uint256 amount);
     event Withdrawal(address indexed user, uint256 amount);
     event FeeCharged(address[] users, uint256[] amounts);
 
     IERC20 public immutable gasToken;
+
     mapping(address => uint256) public balances; // User balances (pre-deposit)
 
-    constructor(address _gasToken) {
+    constructor(address _gasToken, address _gasTank) {
         require(_gasToken != address(0), "Invalid gas token address");
         gasToken = IERC20(_gasToken);
+    }
+
+    function initialize() external initializer {
+        __Ownable_init(msg.sender);
+        __AccessControl_init();
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(ROLE_OPERATOR, msg.sender);
     }
 
     function deposit(uint256 amount) external nonReentrant {
@@ -36,16 +59,31 @@ contract AIGasTank is ReentrancyGuard, Ownable {
         emit Withdrawal(msg.sender, amount);
     }
 
-    function chargeFee(address[] calldata users, uint256[] calldata fees) external onlyOwner nonReentrant {
-        require(users.length == fees.length, "Mismatched input lengths");
-        for (uint256 i = 0; i < users.length; i++) {
-            require(balances[users[i]] >= fees[i], "Insufficient user balance");
-            balances[users[i]] -= fees[i];
+    function reportFees(
+        FeeReport[] calldata reports
+    ) external onlyRole(ROLE_OPERATOR) nonReentrant {
+        for (uint256 i = 0; i < reports.length; i++) {
+            FeeReport calldata r = reports[i];
+            require(balances[r.user] >= r.amount, "Insufficient user balance");
+            balances[r.user] -= r.amount;
+            balances[r.agent] += r.amount;
+            emit FeeCharged(r.user, r.agent, r.amount);
         }
-        emit FeeCharged(users, fees);
     }
 
     function balanceOf(address user) external view returns (uint256) {
         return balances[user];
+    }
+
+    function grantOperator(
+        address operator
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(ROLE_OPERATOR, operator);
+    }
+
+    function revokeOperator(
+        address operator
+    ) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _revokeRole(ROLE_OPERATOR, operator);
     }
 }
